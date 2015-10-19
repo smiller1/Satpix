b6a05cfd7057ab49558206112ccd02ae9829803b
smiller1 <smiller1@hwemail.com>
2015-10-19 16:25:12 -0700

    WORKS PERFECTLY

--- Satpix/src/Satpix.java
+++ Satpix/src/Satpix.java
@@ -3,74 +3,67 @@ import java.util.*;
 
 public class Satpix {
 
-	public static void main(String[] args) throws IOException
-	{
-		boolean[][] booleanArray = fileToBoolArray("satpix.in");
-		
-		//System.out.println(booleanArray);
+	public static void main(String[] args) throws IOException {
+		boolean[][] booleanArray = fileToBoolArray("satpix.txt");
+
 		int sizeOfLargestPasture = 0;
 		int currentSize = 0;
 		for (int i = 0; i < booleanArray.length; i++) {
-			
+
 			for (int j = 0; j < booleanArray[i].length; j++) {
 				currentSize = recursivelyMeasureAndMarkPasture(i, j, booleanArray);
-				
+
 				if (currentSize > sizeOfLargestPasture) {
 					sizeOfLargestPasture = currentSize;
 				}
 			}
 		}
-		
+
 		System.out.println(sizeOfLargestPasture);
-		
+
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("satpix.out")));
 		out.println(sizeOfLargestPasture);
 		out.close();
-		}
-	
-	private static boolean[][] fileToBoolArray(String fileName) throws FileNotFoundException, IOException
-	{
-		// create FileReader/BufferedReader
-		FileReader fr;
-		fr = new FileReader("satpix.txt");
-		//BufferedReader br = new BufferedReader(fr);
-		
+	}
+
+	private static boolean[][] fileToBoolArray(String fileName) throws FileNotFoundException, IOException {
+
 		// create scanner
-		Scanner scan = new Scanner(System.in);
-		
+		Scanner scan = new Scanner(new File(fileName));
+
 		// create 2D boolean array
 		int col = scan.nextInt();
 		int row = scan.nextInt();
 		boolean[][] arr = new boolean[row][col];
-		
+
 		// fill in 2D boolean array
 		for (int i = 0; i < row; i++) {
+			String str = new String(scan.next());
 			for (int j = 0; j < col; j++) {
-				if (fr.read() == ('*'))
+				if (str.charAt(j) == ('*')) {
 					arr[i][j] = true;
-				
-				if (fr.read() == ('.'))
+				} else {
 					arr[i][j] = false;
+				}
 			}
 		}
-		
+		scan.close();
+
 		return arr;
+
 	}
 
-	private static int recursivelyMeasureAndMarkPasture(int row, int col, boolean[][] arr)
-	{
-		
-		if (row >= 0 && col >= 0 && row < arr.length && col < arr[0].length && arr[row][col] == true)
-		{
+	private static int recursivelyMeasureAndMarkPasture(int row, int col, boolean[][] arr) {
+
+		if (row >= 0 && col >= 0 && row < arr.length && col < arr[0].length && arr[row][col] == true) {
 			arr[row][col] = false;
-		   return 1 +
-				recursivelyMeasureAndMarkPasture(--row, col, arr) +
-				recursivelyMeasureAndMarkPasture(++row, col, arr) +
-				recursivelyMeasureAndMarkPasture(row, ++col, arr) +
-				recursivelyMeasureAndMarkPasture(row, --col, arr);
+			return 1 + recursivelyMeasureAndMarkPasture(row + 1, col, arr)
+					+ recursivelyMeasureAndMarkPasture(row - 1, col, arr)
+					+ recursivelyMeasureAndMarkPasture(row, col + 1, arr)
+					+ recursivelyMeasureAndMarkPasture(row, col - 1, arr);
 		}
-		
+
 		return 0;
 	}
-	
+
 }
